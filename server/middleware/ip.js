e51256623a4884b5b3dcf18a3e28587ec16e0441
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-05-21 16:29:10 -0500

    update clientIp middleware

--- server/middleware/ip.js
+++ server/middleware/ip.js
@@ -2,7 +2,8 @@ const loopback = require('loopback');
 
 module.exports = () => (req, res, next) => {
   const context = loopback.getCurrentContext();
-  const clientIp = req.headers[ 'x-forwarded-for' ] || req.connection.remoteAddress;
+  const clientIps = req.headers[ 'x-forwarded-for' ] || req.connection.remoteAddress;
+  const clientIp = clientIps ? clientIps.split(', ', 1)[ 0 ] : null;
 
   if (context && clientIp) context.set('clientIp', clientIp);
 
