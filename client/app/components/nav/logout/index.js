23fab5270966698f61c9179afbda4d69c1116ea8
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-05-21 20:41:27 -0500

    remove unused block

--- client/app/components/nav/logout/index.js
+++ client/app/components/nav/logout/index.js
@@ -1,8 +1,2 @@
-import angular from 'angular';
-
-const { NG_MODULES } = global;
-const MODULE_NAME = 'app.component.nav.logout';
-
-export default MODULE_NAME;
-
-angular.module(MODULE_NAME, [ ...NG_MODULES() ]); // eslint-disable-line new-cap
+import logoutComponent from './logout.component';
+export default logoutComponent;
